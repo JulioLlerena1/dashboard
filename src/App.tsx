0ebcae566df695a775e88776e1c3298132c22ce9
Christian Palma Carpio <chripalm@espol.edu.ec>
2026-07-06 19:50:03 +0000

    componentes

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import './App.css'
 import { Grid } from '@mui/material';
+import HeaderUI from './components/HeaderUI';
+import AlertUI from './components/AlertUI';
 
 function App() {
    return (
       <Grid container spacing={5} sx={{ justifyContent: "left", alignItems: "center" }}>
 
          {/* Encabezado */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Encabezado</Grid>
+         <Grid size={{ xs: 12, md: 12 }}>
+            <HeaderUI></HeaderUI>
+         </Grid>
 
          {/* Alertas */}
-         <Grid size={{ xs: 12, md: 12 }}>Elemento: Alertas</Grid>
+         <Grid  container sx={{ justifyContent: "right", alignItems: "center" }} size={{ xs: 12, md: 12 }}>
+            <AlertUI description="No se preveen lluvias"/>
+         </Grid>
 
          {/* Selector */}
          <Grid size={{ xs: 12, md: 3 }}>Elemento: Selector</Grid>
