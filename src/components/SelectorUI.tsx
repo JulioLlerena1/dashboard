84810bcef677a045b98d84db5befbfb4ba668bb4
Christian Palma Carpio <chripalm@espol.edu.ec>
2026-07-12 19:56:56 +0000

    correcciones

--- src/components/SelectorUI.tsx
+++ src/components/SelectorUI.tsx
@@ -5,13 +5,12 @@ import Select, { type SelectChangeEvent } from '@mui/material/Select';
 import { useState } from 'react';
 
 export default function SelectorUI() {
-   // 1. Declaramos la variable de estado inicializada en string vacío
-   const [ciudad, setCiudad] = useState('');
 
-   // 2. Creamos la función para actualizar el estado cuando el usuario elige una opción
-   const handleChange = (event: SelectChangeEvent) => {
-      setCiudad(event.target.value);
-   };
+   const [cityInput, setCityInput] = useState('');
+
+    const handleChange = (event: SelectChangeEvent<string>) => {
+        setCityInput(event.target.value)
+    };
 
    return (
       <FormControl fullWidth>
@@ -20,16 +19,23 @@ export default function SelectorUI() {
             labelId="city-select-label"
             id="city-simple-select"
             label="Ciudad"
-            value={ciudad} // <-- Pasamos el estado
-            onChange={handleChange} // <-- Pasamos la función de cambio
-         >
-            {/* 3. Agregamos value="" al placeholder para que empate con el estado inicial */}
+            onChange={handleChange} 
+            value={cityInput}
+            >
+
             <MenuItem value="" disabled><em>Seleccione una ciudad</em></MenuItem>
             <MenuItem value={"guayaquil"}>Guayaquil</MenuItem>
             <MenuItem value={"quito"}>Quito</MenuItem>
             <MenuItem value={"manta"}>Manta</MenuItem>
             <MenuItem value={"cuenca"}>Cuenca</MenuItem>
          </Select>
+
+         {cityInput && (
+            <p>
+                Información del clima en <span style={{textTransform: 'capitalize', fontWeight: 'bold'}}>{cityInput}</span>
+            </p>
+        )}
+        
       </FormControl>
    )
 }
